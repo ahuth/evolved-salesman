63e8ad94a83244a6aa5fda8ec7000cd6ee37f72a
Andrew Huth <andrew@huth.me>
2018-07-09 23:07:40 -0700

    Add Point class

---
+++ src/utils/point.js
@@ -0,0 +1,12 @@
+export default class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  distanceFrom(other) {
+    const deltaX = this.position.x - other.position.x;
+    const deltaY = this.position.y - other.position.y;
+    return Math.hypot(deltaX, deltaY);
+  }
+}
