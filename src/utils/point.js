289253c861b881abbd8fe52009280a2e7bc0ba25
Andrew Huth <andrew@huth.me>
2018-07-10 14:48:22 -0700

    De-classify the Point object
    Instead of using a class, have functions that operate on plain old objects.

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,15 +1,13 @@
 import uniqueId from './unique-id';
 
-export default class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.id = uniqueId();
-  }
+export default {
+  create(x, y) {
+    return { x, y, id: uniqueId() };
+  },
 
-  distanceFrom(other) {
-    const deltaX = this.position.x - other.position.x;
-    const deltaY = this.position.y - other.position.y;
+  distance(a, b) {
+    const deltaX = a.x - b.position.x;
+    const deltaY = a.y - b.position.y;
     return Math.hypot(deltaX, deltaY);
-  }
-}
+  },
+};
--- src/utils/random-cities.js
+++ src/utils/random-cities.js
@@ -3,7 +3,7 @@ import random from './random';
 
 export default function randomCities(count, maxX = 100, maxY = 100) {
   return new Array(count).fill(0).map(() => {
-    return new Point(
+    return Point.create(
       random(0, maxX),
       random(0, maxY),
     );
