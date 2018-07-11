e4b1e37b6703a265519cfed4b2ec656c324cd2d5
Andrew Huth <andrew@huth.me>
2018-07-10 20:02:28 -0700

    Refactor the svg path function for readability

--- src/utils/points-to-line-commands.js
+++ src/utils/points-to-line-commands.js
@@ -1,9 +1,15 @@
 export default function pointsToLineCommands(points) {
   return points.reduce((acc, point, index) => {
+    let command;
+
     if (index === 0) {
-      return `${acc} M ${point.x} ${point.y}`;
+      // For the first point, just move the "cursor" to its position.
+      command = 'M';
+    } else {
+      // For subsequent points, draw a line from the current position to a new position.
+      command = 'L';
     }
 
-    return `${acc} L ${point.x} ${point.y}`;
+    return `${acc} ${command} ${point.x} ${point.y}`;
   }, '');
 }
