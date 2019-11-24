e7404f014146ec9c5c0b1e94224674c8d5ed9226
Andrew Huth <andrew@huth.me>
2019-11-24 08:43:12 -0800

    Updates

---
+++ precache-manifest.0d098c2fda50259a39a26f186c645db4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d0f55a2cb389a9c46ed7b380017b87",
+    "url": "/evolved-salesman/index.html"
+  },
+  {
+    "revision": "a4809a2a1267b106b1cd",
+    "url": "/evolved-salesman/static/js/2.f00f5150.chunk.js"
+  },
+  {
+    "revision": "b2eee7ccb13fd24502e3",
+    "url": "/evolved-salesman/static/js/main.6a070bad.chunk.js"
+  },
+  {
+    "revision": "59fca42ed292a0697a7a",
+    "url": "/evolved-salesman/static/js/runtime-main.b2315e1d.js"
+  }
+]);
